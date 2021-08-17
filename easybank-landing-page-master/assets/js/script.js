01b8282b1417bd03b11b4f94277a494cc403b356
SamyrOR <saamyr@live.com>
2021-08-16 23:16:57 -0300

    Animation of hamburguer done, started creation and animation of mobile menu

--- easybank-landing-page-master/assets/js/script.js
+++ easybank-landing-page-master/assets/js/script.js
@@ -1,1 +1,14 @@
-console.log("hello world!");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+header.addEventListener("click", () => {
+  header.classList.toggle("open");
+  if (header.classList.contains("open")) {
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  } else {
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  }
+});
